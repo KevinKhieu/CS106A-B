a9e70b899091a1ba7ed262699075fb3e6a996e41
KevinKhieu <kkhieu@stanford.edu>
2015-08-10 20:40:24 -0500

    Create PokemonConstants.java

---
+++ GraphicsContest/PokemonConstants.java
@@ -0,0 +1,115 @@
+import acm.util.*;
+import acm.program.*;
+import acm.graphics.*;
+import acm.io.*;
+import java.io.*;
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+
+// Global Constants
+public interface PokemonConstants {
+	
+	public static final int GAME_WIDTH = 278;	// Game screen width
+	
+	public static final int GAME_HEIGHT = 224;	// Game screen height
+	
+	public static final int BORDER = 51;		// Border thickness for screen on GBA Adaptor 
+	
+	public static final int HPBarWidth = 82;	// Battle Screen Health Bar
+	
+	public static final int HPBarLength = 5;	// Battle Screen Health Bar
+	
+	public static final int theirHPX = 107;		// Opponent current health bar X position
+	
+	public static final int theirHPY = 96;		// Opponent current health bar Y position
+	
+	public static final int theirMaxHPX = 145;	// Opponent Max health bar X position
+	
+	public static final int theirMaxHPY = 96;	// Opponent Max health bar Y position
+	
+	public static final int theirHPBarX = 200;	// Opponent health bar X position
+	
+	public static final int theirHPBarY = 86;	// Opponent health bar Y position
+	
+	public static final int ourHPBarX = 270;	// Player health bar X position
+	
+	public static final int ourHPBarY = 175;	// Player health bar Y position
+	
+	public static final int ourHPX = 222;		// Player current health bar X position
+	
+	public static final int ourHPY = 185;		// Player current health bar Y position
+	
+	public static final int ourMaxHPX = 260;	// Player Max health bar X position
+	
+	public static final int ourMaxHPY = 185;	// Player Max health bar Y position
+	
+	public static final int BUTTON_WIDTH = 30;
+	
+	public static final int BUTTON_LENGTH = 35;
+	
+	public static final int BUTTON_DECAL_LENGTH = 24;
+	
+	public static final int START_OFFSET = 7;
+	
+	public static final int Line1X = 65;
+	
+	public static final int Line1Y = 232;
+	
+	public static final int Line2X = 65;
+	
+	public static final int ourNameX = 190;
+	
+	public static final int ourNameY = 160;
+	
+	public static final int theirNameX = 80;
+	
+	public static final int theirNameY = 73;
+	
+	public static final int Line2Y = 257;
+	
+	public static final int Line3X = 204;
+	
+	public static final int Line3Y = 232;
+	
+	public static final int Line4X = 204;
+	
+	public static final int Line4Y = 257;
+	
+	public static final int selector1X = 61;
+	
+	public static final int selector1Y = 214;
+	
+	public static final int selector2X = 61;
+	
+	public static final int selector2Y = 240;
+	
+	public static final int selector3X = 189;
+	
+	public static final int selector3Y = 214;
+	
+	public static final int selector4X = 189;
+	
+	public static final int selector4Y = 240;
+	
+	public static final int MAP_STEP_WIDTH = 9;
+	
+	public static final int MAP_STEP_LENGTH = 7;
+	
+	public static final int Xstep = 21;
+	
+	public static final double Ystep = 16;
+	
+	public static final double bottomGameWindow = 275;
+	
+	public static final int OPPONENT_WIDTH=57;
+	
+	public static final int OPPONENT_HEIGHT=63;
+	
+/** The width of the application window */
+	public static final int APPLICATION_WIDTH = 380;
+
+/** The height of the application window */
+	public static final int APPLICATION_HEIGHT = 550;
+}
