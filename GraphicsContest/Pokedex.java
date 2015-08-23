86f4abf7b030686d92d3c00d898b20e9e4ffafad
KevinKhieu <kkhieu@stanford.edu>
2015-08-23 14:04:43 -0500

    Update Pokedex.java

--- GraphicsContest/Pokedex.java
+++ GraphicsContest/Pokedex.java
@@ -1,4 +1,6 @@
-/* A Pokedex serves as this program's mapping of each pokemon to it's corresponding 
+/* File: Pokedex.java
+ * Author: Kevin Khieu
+ * A Pokedex serves as this program's mapping of each pokemon to it's corresponding 
  * Pokemon object, which matches the moves and stats of pokemon in battle to their correct counterpart.
  * One Pokedex object per read in file (so whatever file is read in must have the entire pokemon gropu in it)
  */
@@ -12,7 +14,7 @@ public class Pokedex {
 	// HashMap of Names of Pokemon to Pokemon objects
 	private Map <String, Pokemon>database = new HashMap<String,Pokemon> ();
 	
-	// Upon initializing a Pokedex, we read in a file name line by line. 
+	// Upon initializing a Pokedex, we read in a file called Pokemon-Stats.txt line by line. 
 	// Each line is a Pokemon with stats.
 	public Pokedex (String filename1) {
 		try {
