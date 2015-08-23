336379c31ccc6014626b4274ba564549bea9d6df
KevinKhieu <kkhieu@stanford.edu>
2015-08-23 13:56:08 -0500

    Update Opponent.java

--- GraphicsContest/Opponent.java
+++ GraphicsContest/Opponent.java
@@ -1,12 +1,16 @@
-
+/* Opponent object - there are five opponents that the user faces during his/her fight
+ * through the Elite Four. This class serves to define the behavior of those opponents. */
 
 import acm.util.*;
 import java.io.*;
 import java.util.*;
 
 public class Opponent {
-	private Map <String, Elite> challengers = new HashMap<String,Elite> ();
 	
+	// Maps names of opponents to the actual "Opponent Object", labeled Elites in this program.
+	private Map <String, Elite> challengers = new HashMap<String, Elite> ();
+	
+	// Read in file and the string of challengers.
 	public Opponent(String filename2){
 		try {
 			BufferedReader rd = new BufferedReader(new FileReader(filename2));
@@ -20,9 +24,9 @@ public class Opponent {
 			} catch(IOException ex) {
 				throw new ErrorException(ex);
 			}
-	
 	}
 	
+	// Pulls up the Elite we need.
 	public Elite findEntry(String name) {
 		if (challengers.containsKey(name)){
 			return challengers.get(name);
